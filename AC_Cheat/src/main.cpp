89b1f9cf0e99edbbed4eaf487290086e09c047f5
Baitimus <breachmarket@gmail.com>
2024-11-28 17:49:22 +0100

    commit all

--- AC_Cheat/src/main.cpp
+++ AC_Cheat/src/main.cpp
@@ -4,7 +4,7 @@
 #include "offsets.h"
 #include "entity.h"
 #include "myMath.h"
-
+#include "draw.h"
 
 
 DWORD pID = NULL;
@@ -13,7 +13,7 @@ DWORD baseAddr = NULL;
 float viewMatrix[15];
 myMath mymath;
 
-
+myMath::Vec2 screen;
 //classes and structs
 runTimeInfo run;
 entity ent;
@@ -23,19 +23,29 @@ int main()
 {
 	run.setup(pHandle, run.baseAddr);
 	ent.readEntityList(pHandle, run, ent);
-
+	
+	int width;
+	int height;
+	ReadProcessMemory(pHandle, (LPCVOID)(run.baseAddr + 0x191ED8), &width, sizeof(width), NULL);
+	std::cout << "Width = " << width << std::endl;
+	ReadProcessMemory(pHandle, (LPCVOID)(run.baseAddr + 0x191ED8 + 0x4), &height, sizeof(height), NULL);
+	std::cout << "Height = " << height << std::endl;
 
 	ReadProcessMemory(pHandle, (LPCVOID)(run.baseAddr + off.viewMatrix), &viewMatrix, sizeof(viewMatrix), NULL);
 	for (int i = 0; i <= 15; ++i) {
 		std::cout << "viewMatrix[" << i << "] = " << viewMatrix[i] << std::endl;
 	}
+	screen.x = width;
+	screen.y = height;
 
-	if (mymath.worldToScreen(viewMatrix, ent) ){
-		std::cout << "World to screen success" << std::endl;
+	mymath.worldToScreen(viewMatrix, ent, screen);
+	while (true) {
+		std::cout << "Screen X: " << ent.screenX << std::endl;
+		std::cout << "Screen Y: " << ent.screenY << std::endl;
+
+		drawDotOnScreen(screen.x, screen.y);
 	}
-	else {
-		std::cout << "World to screen failed" << std::endl;
-
+	
 
 
 
@@ -53,6 +63,6 @@ int main()
 
 		CloseHandle(pHandle);
 		return 0;
-	}
+	
 
 };
--- AC_Cheat/src/myMath.h
+++ AC_Cheat/src/myMath.h
@@ -23,13 +23,13 @@ public:
 	
 	
 
-   bool worldToScreen(float viewMatrix[15], entity& ent) {
+   bool worldToScreen(float viewMatrix[15], entity& ent, myMath::Vec2 screen) {
         // Assuming ent has headX, headY, headZ for the position
         myMath::Vec4 clipCoords;
-        clipCoords.x = ent.headX * viewMatrix[0] + ent.headY * viewMatrix[1] + ent.headZ * viewMatrix[2] + viewMatrix[3];
-        clipCoords.y = ent.headX * viewMatrix[4] + ent.headY * viewMatrix[5] + ent.headZ * viewMatrix[6] + viewMatrix[7];
-        clipCoords.z = ent.headX * viewMatrix[8] + ent.headY * viewMatrix[9] + ent.headZ * viewMatrix[10] + viewMatrix[11];
-        clipCoords.w = ent.headX * viewMatrix[12] + ent.headY * viewMatrix[13] + ent.headZ * viewMatrix[14] + viewMatrix[15];
+        clipCoords.x = ent.headX * viewMatrix[0] + ent.headY * viewMatrix[4] + ent.headZ * viewMatrix[8] + viewMatrix[12];
+        clipCoords.y = ent.headX * viewMatrix[1] + ent.headY * viewMatrix[5] + ent.headZ * viewMatrix[9] + viewMatrix[13];
+        clipCoords.z = ent.headX * viewMatrix[2] + ent.headY * viewMatrix[6] + ent.headZ * viewMatrix[10] + viewMatrix[14];
+        clipCoords.w = ent.headX * viewMatrix[3] + ent.headY * viewMatrix[7] + ent.headZ * viewMatrix[11] + viewMatrix[15];
 
         if (clipCoords.w < 0.1f)
             return false;
@@ -40,14 +40,16 @@ public:
         NDC.z = clipCoords.z / clipCoords.w;
 
         // Assuming screen width and height are defined somewhere
-        int screenWidth = 1080;
-        int screenHeight = 1920;
+        int screenWidth = screen.x;
+		int screenHeight = screen.y;
 
-        myMath::Vec2 screen;
+      
         screen.x = (screenWidth / 2 * NDC.x) + (NDC.x + screenWidth / 2);
         screen.y = -(screenHeight / 2 * NDC.y) + (NDC.y + screenHeight / 2);
 		std::cout << "Screen X: " << screen.x << std::endl;
 		std::cout << "Screen Y: " << screen.y << std::endl;
+		ent.screenX = screen.x;
+		ent.screenY = screen.y;
         // You can store or use the screen coordinates as needed
         return true;
     }
